4c39a29a73e54605477a45c9080a3e1f3f344ba5
acer <acer@TMP249-G2-M>
2023-10-04 15:08:14 +0530

    first commit

--- javaproject/src/com/tns/override/Sbi.java
+++ javaproject/src/com/tns/override/Sbi.java
@@ -1,8 +1,7 @@
 package com.tns.override;
 
 public class Sbi extends Rbi {
-
-	  public float getRateofInterest() {
+  public float getRateofInterest() {
 			System.out.println(super.getRateofInterest());
 			return 6.7f;
 		}
